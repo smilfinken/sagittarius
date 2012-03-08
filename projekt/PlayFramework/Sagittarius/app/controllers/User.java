3b5d0b55469482ce2c8e39ff070b2333edf1c7cf
anders <anders.lindsgard@gmail.com>
2012-03-08 20:58:40 +0000

    Continued implementing a REST way of doing things. Why? Can't really say, need to do some reading up on REST....

--- projekt/PlayFramework/Sagittarius/app/controllers/User.java
+++ projekt/PlayFramework/Sagittarius/app/controllers/User.java
@@ -12,52 +12,48 @@ import play.mvc.Controller;
  * @author ancla
  */
 public class User extends Controller {
-    
-    public static void edit(Long userId, String userName, String firstName, String lastName, String eMail) {
-        models.User user;
-        if(userId != null) {
-            user = models.User.findById(userId);
-            notFoundIfNull(user);
-            
-            if (userName != null) {
-                // Edit exising user
-                user.firstName = firstName != null ? firstName : user.firstName;
-                user.lastName = lastName != null ? lastName : user.lastName;
-                user.eMail = eMail != null ? eMail : user.eMail;
-                user.save();
-                flash.success("The user has been updated!");
-                list();
-            } else {
-                render(user);
-            }
+/*    
+POST    /user                                   User.create
+GET     /user                                   User.list
+GET     /user/{id}                              User.read
+PUT     /user/{id}                              User.update
+DELETE  /user/{id}                              User.delete    
+*/
+    public static void create(String userName, String firstName, String lastName, String eMail) {
+        if (userName == null) {
+            render();
         }
+        models.User user;
+        user = new models.User(userName, firstName, lastName, eMail);
+        user.create();
+        flash.success("The user has been created!");                
         list();
     }
-    
-    public static void add(boolean save, String userName, String firstName, String lastName, String eMail) {
-        models.User user;
-        if (save){
-            
-            // Add new user
-            user = new models.User(userName, firstName, lastName, eMail);
-            user.create();
-            flash.success("The user has been created!");                
-            list();
-        } else {
-            render();
-        }    
+    public static void read(Long userId) {
+        models.User user = models.User.findById(userId);
+        notFoundIfNull(user);
+        render("app/views/User/edit.html", user);
     }
-    
+    public static void update(Long userId, String userName, String firstName, String lastName, String eMail) {
+        models.User user = models.User.findById(userId);
+        notFoundIfNull(user);
+        user.firstName = firstName != null ? firstName : user.firstName;
+        user.lastName = lastName != null ? lastName : user.lastName;
+        user.eMail = eMail != null ? eMail : user.eMail;
+        user.save();
+        flash.success("The user has been updated!");
+        list();
+    }
+    public static void delete(Long userId) {
+        if (userId != null) {
+            models.User.delete("id=?", userId);
+        }
+        flash.success("The user has been deleted!");
+        list();    
+    }
     public static void list() {
         List<models.User> users = models.User.all().fetch();
         System.out.println("Listing all users: " + users.size());
         render(users);
-    }
-    
-    public static void delete(Long userId){
-        if (userId != null) {
-            models.User.delete("id=?", userId);
-        }
-        list();
     }
 }

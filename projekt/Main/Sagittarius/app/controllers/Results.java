90c6faa4c85f6dcfc9a9741d15ff63b89568c08b
johan <smilfinken@users.noreply.github.com>
2012-05-06 15:14:19 +0000

    added functionality to export results to csv file

--- projekt/Main/Sagittarius/app/controllers/Results.java
+++ projekt/Main/Sagittarius/app/controllers/Results.java
@@ -4,11 +4,14 @@
  */
 package controllers;
 
-import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.SecureRandom;
 import java.util.*;
 import models.Competitor;
 import models.Result;
+import org.apache.commons.io.output.FileWriterWithEncoding;
 import play.mvc.Controller;
 
 /**
@@ -69,7 +72,7 @@ public class Results extends Controller {
     }
 
     private static List<Competitor> sortResults(List<Competitor> pCompetitors) {
-        final List<String> classOrder = Arrays.asList("C", "D", "V", "B", "A", "R");
+        final List<String> classOrder = Arrays.asList("C", "D", "V", "B", "A", "R", "S");
 
         class ResultListComparator implements Comparator<Competitor> {
 
@@ -86,13 +89,11 @@ public class Results extends Controller {
                     String classB = B.competitorClass.substring(0, B.competitorClass.length() - 1);
                     int classSort = classOrder.indexOf(classA) - classOrder.indexOf(classB);
 
-                    if (classSort != 0){
+                    if (classSort != 0) {
                         return classSort;
-                    }
-                    else
-                    {
-                        String rankA = A.competitorClass.substring(A.competitorClass.length()-1);
-                        String rankB = B.competitorClass.substring(B.competitorClass.length()-1);
+                    } else {
+                        String rankA = A.competitorClass.substring(A.competitorClass.length() - 1);
+                        String rankB = B.competitorClass.substring(B.competitorClass.length() - 1);
 
                         return rankA.compareTo(rankB);
                     }
@@ -142,5 +143,46 @@ public class Results extends Controller {
 
     public static void enter() {
         render();
+    }
+
+    public static void export() throws IOException {
+        List<Competitor> results = Competitor.all().fetch();
+
+        File resultsFile = File.createTempFile("sgt", "csv");
+        resultsFile.deleteOnExit();
+
+        try (FileWriter resultsWriter = new FileWriter(resultsFile)) {
+            String header1 = ";";
+            String header2 = "Namn;Klass";
+            for (int i = 1; i <= 6; i++) {
+                header1 += String.format(";\"Station %d\";;", i);
+                header2 += String.format(";\"Träff\";\"Figurer\";\"Poäng\"", i);
+            }
+            header1 += ";Summa;;;;\n";
+            header2 += ";\"Träff\";\"Figurer\";\"Totalt\";\"Poäng\"\n";
+            resultsWriter.write(header1 + header2);
+
+            for (Competitor competitor : sortResults(results)) {
+                String line = String.format("\"%s\";\"%s\"", competitor.competitorName, competitor.competitorClass);
+                int totalHits = 0;
+                int totalTargets = 0;
+                int totalPoints = 0;
+
+                for (Result result : competitor.competitorResults) {
+                    line += String.format(";%d;%d;%d", result.hits, result.targets, result.points);
+                    totalHits += result.hits;
+                    totalTargets += result.targets;
+                    totalPoints += result.points;
+                }
+
+                line += String.format(";%d;%d;%d;%d\n", totalHits, totalTargets, totalHits + totalTargets, totalPoints);
+                resultsWriter.write(line);
+            }
+        }
+
+        response.setHeader("Content-Length", String.format("%d", resultsFile.length()));
+        response.setHeader("Content-Type", "text/csv; charset=iso-8859-1");
+        response.setHeader("Content-Disposition", "attachment; filename=out.csv");
+        renderBinary(resultsFile);
     }
 }

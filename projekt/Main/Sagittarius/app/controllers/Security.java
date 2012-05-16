4b0b44ecd19a1749c54c74b5a740cdde3719246e
johan <smilfinken@users.noreply.github.com>
2012-05-16 15:35:54 +0000

    consolidating local changes

--- projekt/Main/Sagittarius/app/controllers/Security.java
+++ projekt/Main/Sagittarius/app/controllers/Security.java
@@ -1,26 +1,26 @@
 package controllers;
 
-import models.*;
+import models.User;
 
 public class Security extends Secure.Security {
 
     static boolean authenticate(String username, String password) {
         return User.connect(username, password) != null;
     }
-    
+
     static boolean check(String profile) {
         if("basic".equals(profile)) {
         	return true;
         }
         return false;
     }
-    
+
     static void onDisconnected() {
         Application.index();
     }
-    
+
     static void onAuthenticated() {
         Application.index();
     }
-    
+
 }

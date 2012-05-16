0c0641abc6d88169742461471d263eff7d0f7a6e
johan <johan@f99f5340-55b5-4d58-be29-a39aabc9f24b>
2012-05-16 13:52:32 +0000

    halfway-checkin of stage editing

--- sagittarius/projekt/Main/Sagittarius/app/controllers/Stages.java
+++ sagittarius/projekt/Main/Sagittarius/app/controllers/Stages.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.List;
 import models.Competition;
 import models.Stage;
+import models.Target;
 import play.mvc.Controller;
 import play.mvc.With;
 
@@ -19,7 +20,10 @@ public class Stages extends Controller {
 		render(stages);
 	}
 
-	public static void deleteTargetGroup(long competitionID, long stageID, long targetGroupID) {
-		render();
+	public static void deleteTargetGroup(long competitionID, long stageID, long targetGroupID, int stageIndex) {
+		Competition competition = Competition.findById(competitionID);
+		Stage stage = Stage.findById(stageID);
+		List<Target> targets = Target.all().fetch();
+		renderTemplate("competitions/editstage.html",competition, stage, targets, stageIndex);
 	}
 }

b0da3e5123f3e24bfe96b6779f7c20aa422c17db
Anders <Anders@f99f5340-55b5-4d58-be29-a39aabc9f24b>
2012-05-22 09:09:51 +0000

    Refined the signup functionality.
    Includes example of:
    
    - Form Validation
    - I18N Properties, i.e. localization

--- sagittarius/projekt/Main/Sagittarius/app/controllers/PendingUser.java
+++ sagittarius/projekt/Main/Sagittarius/app/controllers/PendingUser.java
@@ -1,7 +1,9 @@
 	package controllers;
 
 import models.User;
+import play.data.validation.Email;
 import play.data.validation.Required;
+import play.i18n.Messages;
 import play.mvc.Controller;
 
 /**
@@ -14,11 +16,14 @@ public class PendingUser extends Controller {
     	render();
     }
 
-    public static void doSignup(@Required String firstname, @Required String surname, String cardnumber, @Required String email, @Required String password, @Required String passwordVerification) {
-    	if (!password.equals(passwordVerification)) {
-    		/*TODO: Store in flash and redirect to posting page*/
-    		signup();
+    public static void doSignup(@Required String firstname, @Required String surname, String cardnumber, @Required @Email String email, @Required String password, @Required String passwordVerification) {
+    	validation.equals(Messages.get("controller.pendinguser.password"), password, Messages.get("controller.pendinguser.passwordVerification"), passwordVerification);
+    	if(validation.hasErrors()) {
+    	       params.flash(); // add http parameters to the flash scope
+    	       validation.keep(); // keep the errors for the next request
+    	       signup();
     	}
+    	
     	User user = new User( firstname,  surname,  cardnumber, email,  password);
     	user.create();
     	Security.authenticate(email, password);

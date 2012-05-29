7e3022fd86e707e941f95ff3615df1d7e06266c4
smilfinken <smilfinken@users.noreply.github.com>
2012-05-29 15:36:05 +0000

    a not-so-pretty hack to deal with the HTML5 limitations of IE probably not complete, and we need a better solution as well

--- projekt/Main/Sagittarius/app/Bootstrap.java
+++ projekt/Main/Sagittarius/app/Bootstrap.java
@@ -1,4 +1,7 @@
 
+import controllers.Security;
+import java.util.List;
+import models.*;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import play.test.Fixtures;
@@ -10,19 +13,44 @@ import play.test.Fixtures;
 @OnApplicationStart
 public class Bootstrap extends Job {
 
-    @Override
-    public void doJob() {
-        // clean up default data
-        Fixtures.deleteDatabase();
-        Fixtures.loadModels("Defaults/competition-types.yml");
-        Fixtures.loadModels("Defaults/scoring-types.yml");
-        Fixtures.loadModels("Defaults/categories.yml");
-        Fixtures.loadModels("Defaults/divisions.yml");
-        Fixtures.loadModels("Defaults/ranks.yml");
-        Fixtures.loadModels("Defaults/target-models.yml");
+	@Override
+	public void doJob() {
+		// always check before loading fixtures now that we're sometimes using a pre-loaded db
 
-        // load dummy data
-        Fixtures.loadModels("Testdata/dummy-users.yml");
-        Fixtures.loadModels("Testdata/dummy-competitions.yml");
-    }
+		// load semi-static parameters
+		if (CompetitionType.count() == 0) {
+			Fixtures.loadModels("Defaults/competition-types.yml");
+		}
+		if (ScoringType.count() == 0) {
+			Fixtures.loadModels("Defaults/scoring-types.yml");
+		}
+		if (Category.count() == 0) {
+			Fixtures.loadModels("Defaults/categories.yml");
+		}
+		if (Division.count() == 0) {
+			Fixtures.loadModels("Defaults/divisions.yml");
+		}
+		if (Rank.count() == 0) {
+			Fixtures.loadModels("Defaults/ranks.yml");
+		}
+		if (TargetModel.count() == 0) {
+			Fixtures.loadModels("Defaults/target-models.yml");
+		}
+
+		// load dummy data
+		if (User.count() == 0) {
+			Fixtures.loadModels("Testdata/dummy-users.yml");
+		} else {
+			// try to compensate for old data not having hashed passwords
+			List<User> users = User.all().fetch();
+			for (User user : users){
+				if ("123".equals(user.password)){
+					user.password = Security.hashPassword("123");
+				}
+			}
+		}
+		if (Competition.count() == 0) {
+			Fixtures.loadModels("Testdata/dummy-competitions.yml");
+		}
+	}
 }
---
+++ projekt/Main/Sagittarius/app/controllers/Checks.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Http.Header;
+import play.mvc.With;
+
+/**
+ *
+ * @author johan
+ */
+@With(Secure.class)
+public class Checks extends Controller {
+
+	public static boolean checkMSIE(){
+		boolean result = false;
+
+		Header userAgent = request.headers.get("user-agent");
+		if (userAgent != null){
+			result = userAgent.toString().contains("MSIE");
+		}
+
+		return result;
+	}
+}

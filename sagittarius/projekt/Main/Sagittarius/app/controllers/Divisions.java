159f330a9d75cb1476ab67e1ee4917d9be0e21bf
smilfinken <smilfinken@f99f5340-55b5-4d58-be29-a39aabc9f24b>
2012-06-01 14:10:29 +0000

    - user profile edit form (no save feature yet ;) ) - administration of "fixtures" pretty much completed

--- sagittarius/projekt/Main/Sagittarius/app/controllers/Divisions.java
+++ sagittarius/projekt/Main/Sagittarius/app/controllers/Divisions.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import models.Category;
+import models.CompetitionType;
 import models.Division;
 import play.mvc.Controller;
 
@@ -16,6 +19,43 @@ public class Divisions extends Controller {
 	}
 
 	@Check("admin")
-	public static void edit(long divisionID) {
+	public static void edit(long divisionID, String division, boolean ranks, long[] validCompetitionTypes, long[] validCategories, String useraction) {
+		Division item = Division.findById(divisionID);
+
+		if (params._contains("useraction")) {
+			switch (useraction) {
+				case "save":
+					List<CompetitionType> newCTs = new ArrayList<>();
+					for (long i : validCompetitionTypes) {
+						CompetitionType ct = CompetitionType.findById(i);
+						if (ct != null) {
+							newCTs.add(ct);
+						}
+					}
+					List<Category> newCs = new ArrayList<>();
+					for (long i : validCategories) {
+						Category c = Category.findById(i);
+						if (c != null) {
+							newCs.add(c);
+						}
+					}
+					item.division = division;
+					item.ranks = ranks;
+					item.categories = newCs;
+					item.competitionTypes = newCTs;
+					item.save();
+					list();
+					break;
+				case "delete":
+					item.delete();
+					list();
+					break;
+			}
+
+		}
+
+		List<CompetitionType> competitionTypes = CompetitionType.all().fetch();
+		List<Category> categories = Category.all().fetch();
+		render(item, competitionTypes, categories);
 	}
 }

b94d8c8d5d23bc531d8b75f6c1bb5cc5ea117ba5
lindsgard <lindsgard@f99f5340-55b5-4d58-be29-a39aabc9f24b>
2012-06-10 20:42:04 +0000

    Watermark.... =)

--- sagittarius/projekt/Main/Sagittarius/public/javascripts/global.js
+++ sagittarius/projekt/Main/Sagittarius/public/javascripts/global.js
@@ -1,3 +1,34 @@
 $("html, body").ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
 	$(this).html( "Triggered ajaxError handler.<br/>" + jqXHR.responseText );
 });
+
+$(window).bind('resize', function() {
+	atResizing();	
+}).trigger('resize');
+
+
+function atResizing() {
+
+	// Text size
+	var preferredWidth = 1024;
+	var displayWidth = $(window).width();
+	var widthPercentage = displayWidth / preferredWidth;
+	var newFontSize = Math.floor(40 * widthPercentage) - 1;
+	$('.tilted').css("font-size", newFontSize+'px');
+	
+	// Text position
+	var displayHeight = $(window).height();
+	var textHeight = $('.tilted').height();
+	var ratioPercent = textHeight / displayHeight;
+	ratioPercent = Math.floor(ratioPercent*2*100);
+	var presentTop = superTop; // Global, defined below
+	presentTop = (''+presentTop).substring(0, 2);
+	var top = Math.floor(Number(presentTop)+Number(ratioPercent))+'%'
+	$('.tilted').css('top', top);
+};
+
+var superTop;
+$(document).ready(function(){
+	superTop = '50%';
+	atResizing();
+});

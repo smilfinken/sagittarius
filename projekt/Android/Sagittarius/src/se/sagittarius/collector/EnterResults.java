84a74ba8df927878ad641dbf2bb86dd8b82dc71b
smilfinken <smilfinken@users.noreply.github.com>
2012-07-11 21:52:22 +0000

    added some UI and mock data

--- projekt/Android/Sagittarius/src/se/sagittarius/collector/EnterResults.java
+++ projekt/Android/Sagittarius/src/se/sagittarius/collector/EnterResults.java
@@ -3,6 +3,10 @@ package se.sagittarius.collector;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 /**
@@ -11,14 +15,85 @@ import android.widget.TextView;
  */
 public class EnterResults extends Activity {
 
+	Bundle data;
+	Bundle results = Bundle.EMPTY;
+	private int currentCompetitor = 0;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.stage);
 
 		Intent intent = getIntent();
-		String message = intent.getStringExtra(SelectStage.EXTRA_MESSAGE);
+		data = intent.getBundleExtra(SelectStage.BUNDLED_DATA);
+
 		TextView stageTitle = (TextView) findViewById(R.id.stage_title);
-		stageTitle.setText(message);
+		stageTitle.setText(data.getString(SelectStage.STAGE_LABEL));
+
+		nextCompetitor(null);
+
+		try {
+			NumberPicker hitsView = (NumberPicker) findViewById(R.id.score_hits);
+			//hitsView.setWrapSelectorWheel(false);
+			hitsView.setMinValue(0);
+			hitsView.setMaxValue(6);
+		} catch (Error e) {
+			Log.e(this.getClass().toString(), e.toString(), new Throwable(e));
+		}
+
+		try {
+			NumberPicker targetsView = (NumberPicker) findViewById(R.id.score_targets);
+			//targetsView.setWrapSelectorWheel(false);
+			targetsView.setMinValue(0);
+			targetsView.setMaxValue(data.getInt(SelectStage.STAGE_TARGETCOUNT));
+		} catch (Error e) {
+			Log.e(this.getClass().toString(), e.toString(), new Throwable(e));
+		}
+
+		try {
+			NumberPicker pointsView = (NumberPicker) findViewById(R.id.score_points);
+			//pointsView.setWrapSelectorWheel(true);
+			pointsView.setMinValue(0);
+			pointsView.setMaxValue(50);
+		} catch (Error e) {
+			Log.e(this.getClass().toString(), e.toString(), new Throwable(e));
+		}
+	}
+
+	@Override
+	public void finish() {
+		// prepare results bundle
+		results = new Bundle();
+		results.putInt(SelectStage.STAGE_INDEX, data.getInt(SelectStage.STAGE_INDEX));
+
+		// return data to calling activity
+		Intent result = new Intent();
+		result.putExtra(SelectStage.BUNDLED_DATA, results);
+		setResult(RESULT_OK, result);
+
+		super.finish();
+	}
+
+	public void nextCompetitor(View view) {
+		String[] competitorList = data.getStringArray(SelectStage.COMPETITOR_LIST);
+		if (currentCompetitor >= competitorList.length) {
+			setResult(RESULT_OK);
+			this.finish();
+		} else {
+			TextView competitorName = (TextView) findViewById(R.id.competitor);
+			competitorName.setText(String.format("%s %d (%s)", getResources().getString(R.string.competitor), currentCompetitor + 1, competitorList[currentCompetitor++]));
+
+			View pointsContainer = (View) findViewById(R.id.container_points);
+			if (!data.getBoolean(SelectStage.STAGE_POINTS)) {
+				pointsContainer.setVisibility(View.INVISIBLE);
+			} else {
+				pointsContainer.setVisibility(View.VISIBLE);
+			}
+			//pointsContainer.setVisibility(data.getBoolean(SelectStage.STAGE_POINTS) ? 1 : 0);
+			if (currentCompetitor >= competitorList.length) {
+				Button nextButton = (Button) findViewById(R.id.next_competitor);
+				nextButton.setText(getResources().getString(R.string.next_stage));
+			}
+		}
 	}
 }
--- projekt/Android/Sagittarius/src/se/sagittarius/collector/SelectStage.java
+++ projekt/Android/Sagittarius/src/se/sagittarius/collector/SelectStage.java
@@ -4,26 +4,33 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
+import de.quist.app.errorreporter.ExceptionReporter;
 
 public class SelectStage extends Activity {
 
-	public final static String EXTRA_MESSAGE = "se.sagittarius.collector.MESSAGE";
+	public final static String BUNDLED_DATA = "se.sagittarius.collector.BUNDLED_DATA";
+	public final static String COMPETITOR_LIST = "se.sagittarius.collector.COMPETITOR_LIST";
+	public final static String STAGE_INDEX = "se.sagittarius.collector.STAGE_INDEX";
+	public final static String STAGE_LABEL = "se.sagittarius.collector.STAGE_LABEL";
+	public final static String STAGE_POINTS = "se.sagittarius.collector.STAGE_POINTS";
+	public final static String STAGE_TARGETCOUNT = "se.sagittarius.collector.STAGE_TARGETCOUNT";
+	static final int ENTER_RESULTS = 10001;
 
 	/**
 	 * Called when the activity is first created.
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		ExceptionReporter reporter = ExceptionReporter.register(this);
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
 		TextView selectTitle = (TextView) findViewById(R.id.select_title);
-		selectTitle.setText("Patrull 1");
+		selectTitle.setText(getSquadLabel());
 
 		ListView stages = (ListView) findViewById(R.id.list_stages);
 		stages.setOnItemClickListener(new OnItemClickListener() {
@@ -36,11 +43,73 @@ public class SelectStage extends Activity {
 		stages.setAdapter(adapter);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private String getSquadLabel() {
+		return getResources().getString(R.string.dummy_squadlabel);
+	}
+
+	private int getStageCount() {
+		ListView stages = (ListView) findViewById(R.id.list_stages);
+		return stages.getAdapter().getCount();
+	}
+
+	private String getStageLabel(int position) {
+		ListView stages = (ListView) findViewById(R.id.list_stages);
+		return stages.getAdapter().getItem(position).toString();
+	}
+
+	private int getStageTargetCount(int position) {
+		return getResources().getIntArray(R.array.dummy_targets)[position];
+	}
+
+	private boolean getStagePoints(int position) {
+		return Boolean.valueOf(getResources().getStringArray(R.array.dummy_points)[position]);
+	}
+
+	private String[] getCompetitors() {
+		return getResources().getStringArray(R.array.dummy_squad);
+	}
+
 	public void enterResults(AdapterView<?> stages, int position) {
-		Intent intent = new Intent(this, EnterResults.class);
-		String message = stages.getAdapter().getItem(position).toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
+		if (position >= 0 && position <= getStageCount() - 1) {
+			Intent intent = new Intent(this, EnterResults.class);
+			Bundle data = new Bundle();
+
+			// pass the current stage index (using position from ListView)
+			data.putInt(STAGE_INDEX, position);
+
+			// pass the current stage name to the new activity
+			data.putString(STAGE_LABEL, getStageLabel(position));
+
+			// pass the target count for the current stage to the new activity
+			data.putInt(STAGE_TARGETCOUNT, getStageTargetCount(position));
+
+			// pass the status of targets with point values on them to the new activity
+			data.putBoolean(STAGE_POINTS, getStagePoints(position));
+
+			// pass the competitor names to the new activity
+			data.putStringArray(COMPETITOR_LIST, getCompetitors());
+
+			intent.putExtra(BUNDLED_DATA, data);
+			startActivityForResult(intent, ENTER_RESULTS);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK && requestCode == ENTER_RESULTS) {
+			if (data.hasExtra(BUNDLED_DATA)) {
+				Bundle result = data.getBundleExtra(BUNDLED_DATA);
+				ListView stages = (ListView) findViewById(R.id.list_stages);
+				enterResults(stages, result.getInt(STAGE_INDEX) + 1);
+			}
+		}
 	}
 
 	public void exitApp(View view) {

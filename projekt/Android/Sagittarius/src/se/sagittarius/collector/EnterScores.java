4c34690c804f4e525cf18c492659f888e93c5af8
smilfinken <smilfinken@users.noreply.github.com>
2012-07-13 09:28:35 +0000

    results entry (without persistent storage) implemented Issue #SAGITTARIUS-75 - Create an android app for off-line scoring

--- projekt/Android/Sagittarius/src/se/sagittarius/collector/EnterScores.java
+++ projekt/Android/Sagittarius/src/se/sagittarius/collector/EnterScores.java
@@ -18,7 +18,7 @@ public class EnterScores extends Activity {
 	Bundle data;
 	Bundle results = Bundle.EMPTY;
 	private int currentStage;
-	private int currentCompetitor = 0;
+	private int nextCompetitor = 0;
 	Score[] scores;
 
 	@Override
@@ -92,31 +92,38 @@ public class EnterScores extends Activity {
 
 	private int[] getHitsArray() {
 		int[] values = new int[scores.length];
-		for (int i = 0; i < scores.length; i++) {
-			values[i] = scores[i].hits;
+		for (int competitorIndex = 0; competitorIndex < scores.length; competitorIndex++) {
+			values[competitorIndex] = scores[competitorIndex].hits;
 		}
 		return values;
 	}
 
 	private int[] getTargetsArray() {
 		int[] values = new int[scores.length];
-		for (int i = 0; i < scores.length; i++) {
-			values[i] = scores[i].targets;
+		for (int competitorIndex = 0; competitorIndex < scores.length; competitorIndex++) {
+			values[competitorIndex] = scores[competitorIndex].targets;
 		}
 		return values;
 	}
 
 	private int[] getPointsArray() {
 		int[] values = new int[scores.length];
-		for (int i = 0; i < scores.length; i++) {
-			values[i] = scores[i].points;
+		for (int competitorIndex = 0; competitorIndex < scores.length; competitorIndex++) {
+			values[competitorIndex] = scores[competitorIndex].points;
 		}
 		return values;
 	}
 
 	public void nextCompetitor(View view) {
 		String[] competitorList = getCompetitors();
-		if (currentCompetitor >= competitorList.length) {
+
+		// collect current score unless this is the first call
+		if (nextCompetitor > 0) {
+			scores[nextCompetitor - 1] = new Score(getHits(), getTargets(), getPoints());
+		}
+
+		// check for last competitor
+		if (nextCompetitor >= competitorList.length) {
 			// prepare results bundle
 			results = new Bundle();
 			results.putInt(SelectStage.STAGE_INDEX, currentStage);
@@ -124,25 +131,36 @@ public class EnterScores extends Activity {
 			results.putIntArray(SelectStage.SCORING_TARGETS, getTargetsArray());
 			results.putIntArray(SelectStage.SCORING_POINTS, getPointsArray());
 
-			// return data to calling activity
-			Intent data = new Intent();
-			data.putExtra(SelectStage.BUNDLED_DATA, results);
-			setResult(RESULT_OK, data);
+			// return result to calling activity
+			Intent result = new Intent();
+			result.putExtra(SelectStage.BUNDLED_DATA, results);
+			setResult(RESULT_OK, result);
 
+			// finish the activity
 			this.finish();
 		} else {
-			scores[currentCompetitor] = new Score(getHits(), getTargets(), getPoints());
-			TextView competitorName = (TextView) findViewById(R.id.competitor);
-			competitorName.setText(String.format("%s %d (%s)", getResources().getString(R.string.competitor), currentCompetitor + 1, competitorList[currentCompetitor++]));
+			// reset values
+			NumberPicker hits = (NumberPicker) findViewById(R.id.score_hits);
+			hits.setValue(0);
+			NumberPicker targets = (NumberPicker) findViewById(R.id.score_targets);
+			targets.setValue(0);
+			NumberPicker points = (NumberPicker) findViewById(R.id.score_points);
+			points.setValue(0);
 
+			// set label for next competitor
+			TextView competitorName = (TextView) findViewById(R.id.competitor);
+			competitorName.setText(String.format("%s %d (%s)", getResources().getString(R.string.competitor), nextCompetitor + 1, competitorList[nextCompetitor]));
+
+			// set visibility on points input
 			View pointsContainer = (View) findViewById(R.id.container_points);
-			if (!data.getBoolean(SelectStage.STAGE_POINTS)) {
+			if (!data.getBoolean(SelectStage.STAGE_HASPOINTS)) {
 				pointsContainer.setVisibility(View.INVISIBLE);
 			} else {
 				pointsContainer.setVisibility(View.VISIBLE);
 			}
-			//pointsContainer.setVisibility(data.getBoolean(SelectStage.STAGE_POINTS) ? 1 : 0);
-			if (currentCompetitor >= competitorList.length) {
+
+			// increase index and check for last competitor
+			if (++nextCompetitor >= competitorList.length) {
 				Button nextButton = (Button) findViewById(R.id.next_competitor);
 				nextButton.setText(getResources().getString(R.string.next_stage));
 			}
